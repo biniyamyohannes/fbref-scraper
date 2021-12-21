f44b9e5ddae4d8aecdd03ec6e7bff45fb19e4a41
Biniyam Yohannes <biniyamyohannes.by@gmail.com>
2021-12-21 11:40:25 -0700

    added docstrings and type hints

--- crawler.py
+++ crawler.py
@@ -1,27 +1,25 @@
-### crawler.py 
-### Driver script.
-### Iterates over Leagues, Squads and Players and stores their information into a database.
+# crawler.py
+"""Driver program. Iterates over Leagues, Squads, and Players
+ and stores their information into a database."""
 
-
+import re
+import time
+from typing import List
 from urllib.request import urlopen
 from urllib.request import Request
-from urllib.error import HTTPError
-from urllib.error import URLError
 from bs4 import BeautifulSoup
 import database as db
 import playerInfo as pi
 import playerStats as ps
-import time
-import re
 
 # List of leagues to crawl
 leagues = ['https://fbref.com/en/comps/12/La-Liga-Stats',
-		   'https://fbref.com/en/comps/13/Ligue-1-Stats',
-		   'https://fbref.com/en/comps/9/Premier-League-Stats',
-		   'https://fbref.com/en/comps/20/Bundesliga-Stats',
-		   'https://fbref.com/en/comps/11/Serie-A-Stats']
+           'https://fbref.com/en/comps/13/Ligue-1-Stats',
+           'https://fbref.com/en/comps/9/Premier-League-Stats',
+           'https://fbref.com/en/comps/20/Bundesliga-Stats',
+           'https://fbref.com/en/comps/11/Serie-A-Stats']
 
-# List of tables to collect 
+# List of tables to collect per player
 tables = [
     'stats_keeper_dom_lg',
     'stats_keeper_adv_dom_lg',
@@ -34,19 +32,27 @@ tables = [
     'stats_possession_dom_lg',
     'stats_playing_time_dom_lg',
     'stats_misc_dom_lg',
-    ]
+]
 
-def crawl(leagues):
-    header = ['name', 'position', 'foot', 'height', 'weight', 'dob', 'cityob', 'countryob', 'nt', 'club', 'age'] #needs to be added manually since the formatting is inconsistent
-    outfield_tables = ps.getStatsHeader(getPlayers(getSquads(leagues[0])[0])[0], tables)
-    keeper_tables = ps.getStatsHeader(getPlayers(getSquads(leagues[0])[0])[1], tables)
+def crawl(leagues: List[str]) -> None:
+    """
+    Iteratively crawl a list of soccer leagues and scrape player data.
+    Scrapes all teams in a league and all players in a team.
+
+    Arguments:
+         leagues -- list of URLs of soccer leagues to scrape
+    """
+    header = ['name', 'position', 'foot', 'height', 'weight', 'dob', 'cityob',
+              'countryob', 'nt', 'club', 'age']
+    outfield_tables = ps.getStatsHeader(get_players(get_squads(leagues[0])[0])[0], tables)
+    keeper_tables = ps.getStatsHeader(get_players(get_squads(leagues[0])[0])[1], tables)
     db.createInfoTable(header)
     db.createStatsTables(outfield_tables)
     db.createStatsTables(keeper_tables)
 
     for league in leagues:
-        for squad in getSquads(league):
-            for player in getPlayers(squad):
+        for squad in get_squads(league):
+            for player in get_players(squad):
                 print(pi.scrapeInfo(player))
                 db.addInfo(pi.scrapeInfo(player))
                 db.addStats(ps.scrapeStats(player, tables))
@@ -54,33 +60,49 @@ def crawl(leagues):
                 time.sleep(2.0)
 
 
-def getSquads(league):
-	request = Request(league, headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5)'
-	 'AppleWebKit 537.36 (KHTML, like Gecko) Chrome',
-	 'Accept':'text/html,application/xhtml+xml,application/xml;'
-	 'q=0.9,image/webp,*/*;q=0.8'})
-	html = urlopen(request)
-	soup = BeautifulSoup(html, 'html.parser')
-	links = []
-	for link in soup.find("table").find_all('a', href=re.compile('(\/squads\/)')):
-		links.append(link.attrs['href'])
-	return links
-	
+def get_squads(league: str) -> List[str]:
+    """
+    Crawl a league page and collect all team URLs.
 
-def getPlayers(squad):
-	request = Request('https://fbref.com{}'.format(squad), headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5)'
-	 'AppleWebKit 537.36 (KHTML, like Gecko) Chrome',
-	 'Accept':'text/html,application/xhtml+xml,application/xml;'
-	 'q=0.9,image/webp,*/*;q=0.8'})
-	html = urlopen(request)
-	soup = BeautifulSoup(html, 'html.parser')
-	links = []
-	for link in soup.find("table").find_all('a', href = re.compile('(\/players\/)(.){9}(?!(matchlogs))')):
-		links.append(link.attrs['href'])	
-	return links
+    Arguments:
+         league -- single URL of a league
 
-#crawl(leagues)
+    Returns:
+        List of strings. Each string is a unique team URL.
+    """
+    request = Request(league, headers={'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5)'
+                                                    'AppleWebKit 537.36 (KHTML, like Gecko) Chrome',
+                                       'Accept': 'text/html,application/xhtml+xml,application/xml;'
+                                                 'q=0.9,image/webp,*/*;q=0.8'})
+    html = urlopen(request)
+    soup = BeautifulSoup(html, 'html.parser')
+    links = []
+    for link in soup.find("table").find_all('a', href=re.compile('(\/squads\/)')):
+        links.append(link.attrs['href'])
+    return links
 
 
-ps.scrapeStats('/en/players/1d2d5cc8/Steve-Mandanda', tables)
-db.addStats(ps.scrapeStats(player, tables))
+def get_players(squad: str) -> List[str]:
+    """
+    Crawl a team page and collect all player URLs.
+
+    Arguments:
+         squad -- single URL of a league
+
+    Returns:
+        List of strings. Each string is a unique player URL.
+    """
+    request = Request(f'https://fbref.com{squad}',
+                      headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5)'
+                                             'AppleWebKit 537.36 (KHTML, like Gecko) Chrome',
+                               'Accept': 'text/html,application/xhtml+xml,application/xml;'
+                                         'q=0.9,image/webp,*/*;q=0.8'})
+    html = urlopen(request)
+    soup = BeautifulSoup(html, 'html.parser')
+    links = []
+    for link in soup.find("table").find_all('a', href=re.compile('(\/players\/)(.){9}(?!(matchlogs))')):
+        links.append(link.attrs['href'])
+    return links
+
+if __name__ == "__main__":
+    crawl(leagues)
